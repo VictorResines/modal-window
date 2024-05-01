b67081a08f1fc28e847d2de1db44505271107dae
VictorResines <vic.resines@gmail.com>
2024-04-30 21:20:42 -0600

    Add on click open and close modal functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// First we select all the elements that we are going to need.
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// The functions for events
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Next we loop to do something to the group of buttons.
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
